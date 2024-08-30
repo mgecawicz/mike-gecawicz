4793a8f90fcef0e6a8d3fd86d9f270c74415b705
mgecawicz <44035616+mgecawicz@users.noreply.github.com>
2024-08-29 20:12:09 -0400

    Added image to About page and added media queries to the resume page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,48 +2,39 @@
   <div class="title">
     <h1 v-motion-slide-visible-once-right>About</h1>
   </div>
-  <div
-    class="intro"
-    v-motion
-    :initial="{ opacity: 0, y: 30 }"
-    :enter="{ opacity: 1, y: 0, scale: 1 }"
-    :duration="700"
-  >
-    <h2>
-      I am a seasoned web developer with extensive experience in both front-end
-      and back-end technologies, adept at using a range of programming languages
-      such as JavaScript, Python, and PHP to build robust and dynamic web
-      applications.
-      <br /><br />
-      My expertise extends beyond coding to encompass a deep understanding of
-      digital design, where I craft visually stunning content through both
-      traditional and innovative methods. I leverage Adobe tools to create
-      high-quality graphics and video, while also exploring the creative
-      potential of code with platforms like Processing to generate unique,
-      algorithmically-driven visuals.
-      <br /><br />
-      This blend of technical proficiency and artistic vision allows me to
-      deliver comprehensive digital solutions thatare not only functional but
-      also aesthetically compelling. Explore my portfolio to discover how my
-      skills in web development and digital design come together to create
-      engaging and impactful user experiences.
-    </h2>
-  </div>
-  <div
-    class="intro"
-    v-motion
-    :initial="{ opacity: 0, y: 30 }"
-    :enter="{ opacity: 1, y: 0, scale: 1 }"
-    :duration="700"
-  >
-    <h2>{{ item }}</h2>
+  <div class="container">
+    <div>
+      <img v-bind:src="require('../images/me.jpg')" class="myImage" />
+    </div>
+    <div
+      class="intro"
+      v-motion
+      :initial="{ opacity: 0, y: 30 }"
+      :enter="{ opacity: 1, y: 0, scale: 1 }"
+      :duration="700"
+    >
+      <h2>
+        I am a seasoned web developer with extensive experience in both
+        front-end and back-end technologies, adept at using a range of
+        programming languagessuch as JavaScript, Python, and PHP to build robust
+        and dynamic web applications.
+        <br /><br />
+        My expertise extends beyond coding to encompass a deep understanding of
+        digital design, where I craft visually stunning content through both
+        traditional and innovative methods. I leverage Adobe tools to create
+        high-quality graphics and video, while also exploring the creative
+        potential of code with platforms like Processing to generate unique,
+        algorithmically-driven visuals.
+        <br /><br />
+        This blend of technical proficiency and artistic vision allows me to
+        deliver comprehensive digital solutions thatare not only functional but
+        also aesthetically compelling. Explore my portfolio to discover how my
+        skills in web development and digital design come together to create
+        engaging and impactful user experiences.
+      </h2>
+    </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-import lorem from "../data/text.json";
-const item = lorem.lorem as string;
-</script>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
@@ -56,9 +47,50 @@ export default defineComponent({
 .name {
   line-height: 0.8;
 }
-.intro {
-  display: flex;
-  justify-content: center;
+
+@media screen and (max-width: 768px) {
+  /* When Mobile */
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    overflow: hidden;
+  }
+  .myImage {
+    position: absolute;
+    margin-bottom: 5vw;
+    margin-left: -50vw;
+    max-width: 100vw;
+    filter: opacity(25%);
+  }
+  .intro {
+    display: flex;
+    text-align: left;
+    justify-content: center;
+    margin-left: 2vw;
+    margin-right: 5vw;
+    margin-bottom: 5vw;
+  }
+}
+@media screen and (min-width: 768px) {
+  /* When Desktop */
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  .myImage {
+    margin-left: 5vw;
+    margin-bottom: 5vw;
+    max-width: 30vw;
+  }
+  .intro {
+    display: flex;
+    justify-content: center;
+    margin-left: 2vw;
+    margin-right: 5vw;
+    margin-bottom: 5vw;
+  }
 }
 .tagLine {
   margin-top: 3%;
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -2,16 +2,12 @@
   <div class="title">
     <h1 v-motion-slide-visible-once-right>Resume</h1>
     <div class="contact" v-motion-slide-visible-once-left>
-      <h2 style="font-weight: 1000; font-size: 2.5vw">{{ resume.name }}</h2>
+      <h2 class="myName">{{ resume.name }}</h2>
       <h3 style="color: #444">{{ resume.location }}</h3>
       <h3 style="color: #444">{{ resume.email }}</h3>
     </div>
     <div style="background-color: #219ebc; padding-top: 0.2vw">
-      <h2
-        style="margin-top: 5vw; font-weight: 1000; font-size: 2vw; color: white"
-      >
-        Experience
-      </h2>
+      <h2 class="myExperience">Experience</h2>
       <div
         v-for="item in resume.experience"
         v-bind:key="item.company"
@@ -21,7 +17,7 @@
         <h3>{{ item.location }}</h3>
         <h3>{{ item.position }}</h3>
         <h3>{{ item.duration }}</h3>
-        <h3>{{ item.description }}</h3>
+        <h3 class="description">{{ item.description }}</h3>
         <h3
           v-if="item.publications.length > 0"
           style="margin-left: 5vw; color: #8ecae6"
@@ -86,37 +82,84 @@ export default defineComponent({
 .education-block {
   line-height: 1;
   max-width: 80vw;
-  margin-left: 5vh;
+  margin-left: 5vw;
 }
 .tagLine {
   margin-top: 3%;
   margin-left: 3%;
   line-height: 0.4;
 }
-h2 {
-  font-weight: 300;
-  text-align: left;
-  font-size: 2vw;
-  margin-left: 2vw;
+@media screen and (max-width: 768px) {
+  /* When Mobile */
+  h2 {
+    font-weight: 300;
+    text-align: left;
+    font-size: 50px;
+    margin-left: 2vw;
+  }
+  h3 {
+    text-align: left;
+    font-size: 20px;
+    margin-left: 2vw;
+  }
+  .myName {
+    font-weight: 1000;
+    font-size: 30px;
+  }
+  h4 {
+    text-align: left;
+    margin-top: 0;
+    margin-bottom: 0;
+    font-size: 12px;
+    margin-left: 5vw;
+  }
+  .myExperience {
+    margin-top: 5vw;
+    font-weight: 1000;
+    font-size: 25px;
+    color: white;
+  }
+  .description {
+    font-size: 12px;
+  }
 }
-h3 {
-  text-align: left;
-  font-size: 1.5vw;
-  margin-left: 2vw;
-}
-h4 {
-  text-align: left;
-  margin-top: 0;
-  margin-bottom: 0;
-  font-size: 1vw;
-  margin-left: 5vw;
+@media screen and (min-width: 768px) {
+  /* When Desktop */
+  h2 {
+    font-weight: 300;
+    text-align: left;
+    font-size: 2vw;
+    margin-left: 2vw;
+  }
+  h3 {
+    text-align: left;
+    font-size: 18px;
+    margin-left: 2vw;
+  }
+  .myName {
+    font-weight: 1000;
+    font-size: 30px;
+  }
+  h4 {
+    text-align: left;
+    margin-top: 0;
+    margin-bottom: 0;
+    font-size: 12px;
+    margin-left: 5vw;
+  }
+  .myExperience {
+    margin-top: 5vw;
+    font-weight: 1000;
+    font-size: 25px;
+    color: white;
+  }
 }
 h1 {
   color: white;
   text-align: right;
   margin-bottom: 0;
   margin-right: 2%;
-  margin-top: 0;
+  margin-top: 2%;
   font-size: 10vw;
 }
 </style>
