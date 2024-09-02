080c93610a9709c790266867e1abffea25a0a833
mgecawicz <44035616+mgecawicz@users.noreply.github.com>
2024-09-02 01:22:50 -0400

    Removed conflicting video npm package

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,9 @@ import router from "./router";
 import "highlight.js/styles/atom-one-dark-reasonable.css";
 import "highlight.js/lib/common";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
-import VueVideoPlayer from "@videojs-player/vue";
-import "video.js/dist/video-js.css";
 
 const app = createApp(App);
 app.use(router);
 app.use(hljsVuePlugin);
 app.use(MotionPlugin);
-app.use(VueVideoPlayer);
 app.mount("#app");
